6892eeb7fd8d970e110b20e06935e6347c437a62
Adrian Slade <adrian.slade@basekit.com>
2017-10-25 19:06:51 +0100

    Add basic writeLine implementation

--- examples/add-job.php
+++ examples/add-job.php
@@ -1,6 +1,12 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$client = new FaktoryClient;
-$job = new FaktoryJob("somejob", [1, 2, 3]);
+/*
+ * Use nc to listen for connections
+ *
+ * nc -l 32769
+ */
+
+$client = new BaseKit\Faktory\FaktoryClient;
+$job = new BaseKit\Faktory\FaktoryJob("somejob", [1, 2, 3]);
 $client->push($job);
--- src/FaktoryClient.php
+++ src/FaktoryClient.php
@@ -5,5 +5,14 @@ class FaktoryClient
 {
     public function push(FaktoryJob $job)
     {
+        $this->writeLine('PUSH', json_encode($job));
+    }
+
+    public function writeLine($command, $json)
+    {
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_connect($socket, '172.17.0.1', '32769');
+        socket_write($socket, $command . ' ' . $json);
+        socket_close($socket);
     }
 }
